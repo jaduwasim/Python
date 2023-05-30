66c931f9ee2fe7e91de81aa3d583fd284b996cc2
jaduwasim <washimakram1631@gmail.com>
2023-05-30 17:00:56 +0530

    test.py files added

---
+++ Pattern/p2.py
@@ -0,0 +1,13 @@
+'''
+1111111111
+2222222222
+3333333333
+4444444444
+5555555555
+'''
+
+n = int(input('Enter the Number of rows:'))
+for i in range(1,n+1):
+	for j in range(1,n+1):
+		print(i,end='')
+	print()
--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-from bs4 import BeautifulSoup
-import requests
-# from csv import writer
-# import re
-
-url= "https://www.jotform.com/app/222393442102445/222065692015451"
-page = requests.get(url)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup)
-lists = soup.find_all('div', class_='form-all')
-print(lists)
+myText = "Hello Linkedin"
+print(myText[3:10])
