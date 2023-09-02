517fcbb0b998b8a9c21313a8dff3c16dd6d5ddd5
jaduwasim <washimakram1631@gmail.com>
2023-09-02 09:49:52 +0530

    Templates added

--- demo.py
+++ demo.py
@@ -1,3 +1,9 @@
-from test import demo
+from flask import Flask, render_template
 
-demo()			
+app = Flask(__name__)
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
+if __name__ == '__main__':
+	app.run(port=8000, debug=True)
--- test.py
+++ test.py
@@ -1,34 +1,9 @@
-class car:
-	def __init__(self, car_name, car_model, car_color):
-		self.car_name = car_name
-		self.car_model = car_model
-		self.car_color = car_color
+from flask import Flask, render_template
 
-	def carinfo(self):
-		print(f'Car Name : {self.car_name}, Car Model : {self.car_model} and Color : {self.car_color}')
+app = Flask(__name__)
+@app.route('/home')
+def home():
+	return render_template('home.html')
 
-class Person:
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
-
-	def eatinfo(self):
-		print('Eat Biryani and drink Bear!!!!')
-
-class Employee(Person):
-	def __init__(self, name , age, eno, esal, car):
-		super().__init__(name, age)
-		self.eno = eno
-		self.esal = esal 
-		self.car = car
-
-	def emplinfo(self):
-		print(f'Employee Name {self.name}, Employee: {self.age}')
-		print(f'Employee eno: {self.eno}, Employee Salary: {self.esal}')
-		print(f'Employee Car info')
-		self.car.carinfo()
-
-c = car('Innova','2.5V', 'Grey')
-e = Employee('jadu', 25, 802425, 70000, c)
-e.emplinfo()
-e.eatinfo()
+if __name__ == '__main__':
+	app.run(port=5000, debug=True)
